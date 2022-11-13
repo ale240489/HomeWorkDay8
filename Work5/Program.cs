717efd4561eeb9b9fdf747165f3491e7726f7021
Aleksandr <ale240489@yandex.ru>
2022-11-13 23:40:59 +0300

    Work5

---
+++ Work5/Program.cs
@@ -0,0 +1,54 @@
+﻿// Напишите программу, котоая заполняет спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int[,] SpiralMatrix()
+{
+    int n = 4;
+    int[,] sqareMatrix = new int[n, n];
+    int count = 1;
+    int i = 0;
+    int j = 0;
+
+    while (count <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+    {
+        sqareMatrix[i, j] = count;
+        count++;
+        if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+return sqareMatrix;
+}
+
+void PrintArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+int[,] matrix = SpiralMatrix();
+PrintArray(matrix);
+
+
+
+     
+     
+     
+    
